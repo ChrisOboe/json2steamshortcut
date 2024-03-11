6e70feb609c2411d1b003f79537d54cd1584308f
ChrisOboe <chris@oboe.email>
2024-03-11 18:03:18 +0100

    make output reproducible

--- src/main.go
+++ src/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"hash/fnv"
 	"os"
 
 	"github.com/stephen-fox/steamutil/shortcuts"
 )
+
+func hash(s string) int {
+	h := fnv.New32()
+	h.Write([]byte(s))
+	return int(h.Sum32())
+}
 
 func main() {
 	var s []shortcuts.Shortcut
@@ -15,6 +22,14 @@ func main() {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
+
+	// generate reproducable id if it's not explicitely set
+	for i, shortcut := range s {
+		if shortcut.Id == 0 {
+			s[i].Id = hash(shortcut.AppName)
+		}
+	}
+
 	err = shortcuts.WriteVdfV1(s, os.Stdout)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
